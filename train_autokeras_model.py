af882f5fdb5b7c146b5242020e6a10e49b9070ab
Danny Key <danny@dannydabbles.com>
2020-02-13 19:06:49 -0800

    Refactoring python scripts

--- train_autokeras_model.py
+++ train_autokeras_model.py
@@ -1,40 +1,95 @@
 #!/usr/bin/python
 
 import os
-import shutil
-import time
-import traceback
 from tensorflow import lite
 import tensorflow as tf
 import autokeras as ak
 from datetime import datetime
 from PIL import Image
-import matplotlib.pyplot as plt
-import random
 from IPython.display import display
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import gc
 import numpy as np
 
-project_dir = os.path.dirname(os.path.realpath(__file__))
+image_size = 75
 
-tensorboard_dir=os.path.join(project_dir, "logs/fit", datetime.now().strftime("%Y%m%d-%H%M%S"))
+project_dir = os.path.dirname(os.path.realpath(__file__))
+auto_model_dir = os.path.join(project_dir, "auto_model")
+image_dir = os.path.join(project_dir, "images")
+tensorboard_dir = os.path.join(project_dir, "logs/fit", datetime.now().strftime("%Y%m%d-%H%M%S"))
+
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_dir, histogram_freq=1)
 
-class GarbageCallback(tf.keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs=None):
-        gc.collect()
 
-auto_model_dir = os.path.join(project_dir, "auto_model")
+def generate_data(func):
+    def _generate_data_fn():
+        datagen = ImageDataGenerator(
+            validation_split=0.3,
+            rotation_range=5,
+            width_shift_range=0.02,
+            height_shift_range=0.02,
+            shear_range=0.02,
+            zoom_range=0.02,
+            horizontal_flip=True,
+            fill_mode='nearest')
+        training = datagen.flow_from_directory(
+            os.path.join(image_dir, "train"),
+            shuffle=True,
+            class_mode='categorical',
+            batch_size=50000,
+            subset="training",
+            target_size=(image_size, image_size))
+        validate = datagen.flow_from_directory(
+            os.path.join(image_dir, "train"),
+            shuffle=True,
+            class_mode='categorical',
+            batch_size=20000,
+            subset="validation",
+            target_size=(image_size, image_size))
+        return func(training, validate)
 
-def io_api(x_train, y_train, x_validate, y_validate, x_test, y_test):
+    return _generate_data_fn
+
+
+def unpack_data(func):
+    def _unpack_data_fn(training=None, validate=None):
+        x_validate, y_validate = validate.next()
+
+        x_validate = x_validate.astype('uint8')
+        y_validate = y_validate.astype('uint8')
+
+        split_num = int(len(x_validate) * .25)
+        x_test = x_validate[:split_num]
+        y_test = y_validate[:split_num],
+        x_vaidate = x_validate[split_num:]
+        y_vaidate = y_validate[split_num:]
+
+        label_map = dict((v, k) for k, v in training.class_indices.items())
+        print(label_map)
+        x_train, y_train = training.next()
+        x_train = x_train.astype('uint8')
+        y_train = y_train.astype('uint8')
+
+        print("Data type: {}".format(type(x_train[0][0][0][0])))
+
+        for i in range(min(int(len(x_train)), 10)):
+            print("Label: " + label_map[np.where(y_train[i] == 1)[0][0]])
+            display(Image.fromarray(x_train[i], 'RGB'))
+
+        return func(x_train, y_train, x_validate, y_validate, x_test, y_test)
+
+    return _unpack_data_fn
+
+
+@generate_data
+@unpack_data
+def generate_model(x_train, y_train, x_validate, y_validate, x_test, y_test):
     model = ak.ImageClassifier(
         seed=29,
         max_trials=25,
         directory=auto_model_dir)
     model.fit(x_train, y_train,
               validation_data=(x_validate, y_validate),
-              callbacks=[GarbageCallback(), tensorboard_callback],
+              callbacks=[tensorboard_callback],
               batch_size=32)
     model_name = "autokeras_model_" + datetime.now().strftime("%d_%m_%Y__%H_%M_%S")
     model_path = os.path.join(project_dir, "models", model_name)
@@ -46,70 +101,24 @@ def io_api(x_train, y_train, x_validate, y_validate, x_test, y_test):
     print("INFO: Predictions is {}".format(model.predict(x_test)))
     return model.evaluate(x_test, y_test)
 
-print(tf.config.experimental.list_physical_devices(device_type=None))
-for gpu in tf.config.experimental.list_physical_devices(device_type='GPU'):
-    tf.config.experimental.set_memory_growth(gpu, True) 
 
-image_size = 75
-image_dir = os.path.join(project_dir, "images")
-print("INFO: Training started with size {}".format(image_size))
+def main():
+    print("INFO: Training started with size {}".format(image_size))
 
-#if os.path.exists(auto_model_dir):
-#    print("INFO: Removing saved model directory: {}".format(auto_model_dir))
-#    shutil.rmtree(auto_model_dir)
+    print(tf.config.experimental.list_physical_devices(device_type=None))
+    for gpu in tf.config.experimental.list_physical_devices(device_type='GPU'):
+        tf.config.experimental.set_memory_growth(gpu, True)
 
-datagen = ImageDataGenerator(
-    validation_split=0.3,
-    rotation_range=5,
-    width_shift_range=0.02,
-    height_shift_range=0.02,
-    shear_range=0.02,
-    zoom_range=0.02,
-    horizontal_flip=True,
-    fill_mode='nearest')
-training = datagen.flow_from_directory(
-    os.path.join(image_dir, "train"),
-    shuffle=True,
-    class_mode='categorical',
-    batch_size=50000,
-    subset="training",
-    target_size=(image_size, image_size))
-validate = datagen.flow_from_directory(
-    os.path.join(image_dir, "train"),
-    shuffle=True,
-    class_mode='categorical',
-    batch_size=20000,
-    subset="validation",
-    target_size=(image_size, image_size))
+    # if os.path.exists(auto_model_dir):
+    #    print("INFO: Removing saved model directory: {}".format(auto_model_dir))
+    #    shutil.rmtree(auto_model_dir)
 
-x_validate, y_validate = validate.next()
+    start = datetime.now()
+    print("INFO: Training results: {}".format(generate_model()))
+    end = datetime.now()
+    delta = end - start
+    print("INFO: Training completed in {}".format(str(delta)))
 
-x_validate = x_validate.astype('uint8')
-y_validate = y_validate.astype('uint8')
 
-split_num = int(len(x_validate)*.25)
-x_test = x_validate[:split_num]
-y_test = y_validate[:split_num],
-x_vaidate = x_validate[split_num:]
-y_vaidate = y_validate[split_num:]
-
-label_map = dict((v,k) for k,v in training.class_indices.items())
-print(label_map)
-x_train, y_train = training.next()
-x_train = x_train.astype('uint8')
-y_train = y_train.astype('uint8')
-
-print("Data type: {}".format(type(x_train[0][0][0][0])))
-
-for i in range(min(int(len(x_train)), 10)):
-    print("Label: " + label_map[np.where(y_train[i] == 1)[0][0]])
-    display(Image.fromarray(x_train[i], 'RGB'))
-
-start = datetime.now()
-print("INFO: Training results: {}".format(
-    io_api(x_train, y_train,
-           x_validate, y_validate,
-           x_test, y_test)))
-end = datetime.now()
-delta = end - start
-print("INFO: Training completed in {}".format(str(delta)))
+if __name__ == "__main__":
+    main()
--- train_inception_model.py
+++ train_inception_model.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python
 
 import os
-import shutil
-import time
-import traceback
-import autokeras as ak
 from datetime import datetime
 from PIL import Image
-import matplotlib.pyplot as plt
-import random
 from IPython.display import display
-import gc
 import numpy as np
 from collections import Counter
 
@@ -18,179 +11,195 @@ import tensorflow as tf
 from tensorflow import lite
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
-from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Conv2D, Input
-from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Input
 from tensorflow.keras.optimizers import SGD, RMSprop
-
-project_dir = os.path.dirname(os.path.realpath(__file__))
-
-tensorboard_dir=os.path.join(project_dir, "logs/fit", datetime.now().strftime("%Y%m%d-%H%M%S"))
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_dir, histogram_freq=1)
-
-print(tf.config.experimental.list_physical_devices(device_type=None))
-for gpu in tf.config.experimental.list_physical_devices(device_type='GPU'):
-    tf.config.experimental.set_memory_growth(gpu, True)
 
 image_size = 299
 batch_size = 32
+
+project_dir = os.path.dirname(os.path.realpath(__file__))
+tensorboard_dir = os.path.join(project_dir, "logs/fit", datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_dir, histogram_freq=1)
+
 image_dir = os.path.join(project_dir, 'images')
 
-datagen = ImageDataGenerator(
-    preprocessing_function=preprocess_input,
-    validation_split=0.3,
-    rotation_range=5,
-    width_shift_range=0.02,
-    height_shift_range=0.02,
-    shear_range=0.02,
-    zoom_range=0.02,
-    horizontal_flip=True,
-    fill_mode='nearest')
-training = datagen.flow_from_directory(
-    os.path.join(image_dir, "train"),
-    shuffle=True,
-    class_mode='categorical',
-    batch_size=batch_size,
-    subset="training",
-    target_size=(image_size, image_size))
-validate = datagen.flow_from_directory(
-    os.path.join(image_dir, "train"),
-    shuffle=True,
-    class_mode='categorical',
-    batch_size=batch_size,
-    subset="validation",
-    target_size=(image_size, image_size))
 
-label_map = dict((v,k) for k,v in training.class_indices.items())
-print(label_map)
+def generate_data(func):
+    def _generate_data_fn():
+        datagen = ImageDataGenerator(
+            preprocessing_function=preprocess_input,
+            validation_split=0.3,
+            rotation_range=5,
+            width_shift_range=0.02,
+            height_shift_range=0.02,
+            shear_range=0.02,
+            zoom_range=0.02,
+            horizontal_flip=True,
+            fill_mode='nearest')
+        training = datagen.flow_from_directory(
+            os.path.join(image_dir, "train"),
+            shuffle=True,
+            class_mode='categorical',
+            batch_size=batch_size,
+            subset="training",
+            target_size=(image_size, image_size))
+        validate = datagen.flow_from_directory(
+            os.path.join(image_dir, "train"),
+            shuffle=True,
+            class_mode='categorical',
+            batch_size=batch_size,
+            subset="validation",
+            target_size=(image_size, image_size))
+        return func(training, validate)
 
-num_classes = len(label_map.keys())
-print("Num classes: {}".format(num_classes))
+    return _generate_data_fn()
 
-x_train, y_train = training.next()
 
-counter = Counter(training.classes)
-max_val = float(max(counter.values()))       
-class_weights = {class_id : max_val/num_images for class_id, num_images in counter.items()}
+@generate_data
+def train_model(training, validate):
+    label_map = dict((v, k) for k, v in training.class_indices.items())
+    print(label_map)
 
-print("Data type: {}".format(type(x_train[0][0][0][0])))
+    num_classes = len(label_map.keys())
+    print("Num classes: {}".format(num_classes))
 
-for i in range(min(int(len(x_train)), 10)):
-    print("Label: " + label_map[np.where(y_train[i] == 1)[0][0]])
-    display(Image.fromarray((x_train[i]*127.5+127.5).astype('uint8'), 'RGB'))
+    x_train, y_train = training.next()
 
-start = datetime.now()
-print("INFO: Training started with image size {} and batch size {}".format(image_size, batch_size))
+    counter = Counter(training.classes)
+    max_val = float(max(counter.values()))
+    class_weights = {class_id: max_val / num_images for class_id, num_images in counter.items()}
 
-# create the base pre-trained model
-base_model = InceptionV3(
-    weights='imagenet',
-    layers=tf.keras.layers, # See https://github.com/keras-team/keras/pull/9965#issuecomment-549126009
-    input_tensor=Input(shape=(image_size, image_size, 3)),
-    include_top=False)
+    print("Data type: {}".format(type(x_train[0][0][0][0])))
 
-# add a global spatial average pooling layer
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-# let's add a fully-connected layer
-x = Dense(1024, activation='relu')(x)
-# and a logistic layer -- let's say we have 200 classes
-predictions = Dense(num_classes, activation='softmax')(x)
+    for i in range(min(int(len(x_train)), 10)):
+        print("Label: " + label_map[np.where(y_train[i] == 1)[0][0]])
+        display(Image.fromarray((x_train[i] * 127.5 + 127.5).astype('uint8'), 'RGB'))
 
-# this is the model we will train
-model = Model(inputs=base_model.input, outputs=predictions)
+    # create the base pre-trained model
+    base_model = InceptionV3(
+        weights='imagenet',
+        layers=tf.keras.layers,  # See https://github.com/keras-team/keras/pull/9965#issuecomment-549126009
+        input_tensor=Input(shape=(image_size, image_size, 3)),
+        include_top=False)
 
-# first: train only the top layers (which were randomly initialized)
-# i.e. freeze all convolutional InceptionV3 layers
-for layer in base_model.layers:
-    layer.trainable = False
-    #layer.trainable = True
+    # add a global spatial average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    # let's add a fully-connected layer
+    x = Dense(1024, activation='relu')(x)
+    # and a logistic layer -- let's say we have 200 classes
+    predictions = Dense(num_classes, activation='softmax')(x)
 
-# compile the model (should be done *after* setting layers to non-trainable)
-model.compile(
-    optimizer=RMSprop(learning_rate=0.000005, rho=0.9),
-    loss='categorical_crossentropy',
-    metrics=['accuracy'])
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=predictions)
 
-# train the model on the new data for a few epochs
-history = model.fit(
-    training,
-    class_weight=class_weights,
-    #steps_per_epoch=1,
-    epochs=15,
-    validation_data=validate,
-    #validation_steps=1,
-    callbacks=[tensorboard_callback])
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all convolutional InceptionV3 layers
+    for layer in base_model.layers:
+        layer.trainable = False
+        # layer.trainable = True
 
-# The returned "history" object holds a record
-# of the loss values and metric values during training
-print('\nhistory dict:', history.history)
+    # compile the model (should be done *after* setting layers to non-trainable)
+    model.compile(
+        optimizer=RMSprop(learning_rate=0.000005, rho=0.9),
+        loss='categorical_crossentropy',
+        metrics=['accuracy'])
 
-print(model.summary())
+    # train the model on the new data for a few epochs
+    history = model.fit(
+        training,
+        class_weight=class_weights,
+        # steps_per_epoch=1,
+        epochs=20,
+        validation_data=validate,
+        # validation_steps=1,
+        callbacks=[tensorboard_callback])
 
-model_name = "inception_model_" + datetime.now().strftime("%d_%m_%Y__%H_%M_%S")
-model_path = os.path.join(project_dir, "models", model_name)
-os.makedirs(os.path.dirname(model_path), exist_ok=True)
-model.save(model_path + "_half.h5")
-tflite_model = lite.TFLiteConverter.from_keras_model(model).convert()
-open(model_path + "_half.tflite", "wb").write(tflite_model)
+    # The returned "history" object holds a record
+    # of the loss values and metric values during training
+    print('\nhistory dict:', history.history)
 
-# at this point, the top layers are well trained and we can start fine-tuning
-# convolutional layers from inception V3. We will freeze the bottom N layers
-# and train the remaining top layers.
+    print(model.summary())
 
-# let's visualize layer names and layer indices to see how many layers
-# we should freeze:
-for i, layer in enumerate(base_model.layers):
-    print(i, layer.name)
+    model_name = "inception_model_" + datetime.now().strftime("%d_%m_%Y__%H_%M_%S")
+    model_path = os.path.join(project_dir, "models", model_name)
+    os.makedirs(os.path.dirname(model_path), exist_ok=True)
+    model.save(model_path + "_half.h5")
+    tflite_model = lite.TFLiteConverter.from_keras_model(model).convert()
+    open(model_path + "_half.tflite", "wb").write(tflite_model)
 
-# we chose to train the top 2 inception blocks, i.e. we will freeze
-# the first 249 layers and unfreeze the rest:
-for layer in model.layers[:249]:
-    layer.trainable = False
-for layer in model.layers[249:]:
-    layer.trainable = True
+    # at this point, the top layers are well trained and we can start fine-tuning
+    # convolutional layers from inception V3. We will freeze the bottom N layers
+    # and train the remaining top layers.
 
-# we need to recompile the model for these modifications to take effect
-# we use SGD with a low learning rate
-model.compile(
-    optimizer=SGD(lr=0.00005, momentum=0.9),
-    loss='categorical_crossentropy',
-    metrics=['accuracy'])
+    # let's visualize layer names and layer indices to see how many layers
+    # we should freeze:
+    for i, layer in enumerate(base_model.layers):
+        print(i, layer.name)
 
-# we train our model again (this time fine-tuning the top 2 inception blocks
-# alongside the top Dense layers
-history = model.fit(
-    training,
-    class_weight=class_weights,
-    #steps_per_epoch=1,
-    epochs=15,
-    validation_data=validate,
-    #validation_steps=1,
-    callbacks=[tensorboard_callback])
+    # we chose to train the top 2 inception blocks, i.e. we will freeze
+    # the first 249 layers and unfreeze the rest:
+    for layer in model.layers[:249]:
+        layer.trainable = False
+    for layer in model.layers[249:]:
+        layer.trainable = True
 
-# The returned "history" object holds a record
-# of the loss values and metric values during training
-print('\nhistory dict:', history.history)
+    # we need to recompile the model for these modifications to take effect
+    # we use SGD with a low learning rate
+    model.compile(
+        optimizer=SGD(lr=0.00005, momentum=0.9),
+        loss='categorical_crossentropy',
+        metrics=['accuracy'])
 
-print(model.summary())
+    # we train our model again (this time fine-tuning the top 2 inception blocks
+    # alongside the top Dense layers
+    history = model.fit(
+        training,
+        class_weight=class_weights,
+        # steps_per_epoch=1,
+        epochs=20,
+        validation_data=validate,
+        # validation_steps=1,
+        callbacks=[tensorboard_callback])
 
-model_name = "inception_model_" + datetime.now().strftime("%d_%m_%Y__%H_%M_%S")
-model_path = os.path.join(project_dir, "models", model_name)
-model.save(model_path + ".h5")
-tflite_model = lite.TFLiteConverter.from_keras_model(model).convert()
-open(model_path + ".tflite", "wb").write(tflite_model)
+    # The returned "history" object holds a record
+    # of the loss values and metric values during training
+    print('\nhistory dict:', history.history)
 
-# Evaluate the model on the test data using `evaluate`
-print('\n# Evaluate on test data')
-results = model.evaluate(validate)
-print('test loss, test acc:', results)
+    print(model.summary())
 
-print('\n# Generate predictions for 3 samples')
-print("INFO: Predictions is {}".format(model.predict(validate)))
-print('predictions shape:', predictions.shape)
+    model_name = "inception_model_" + datetime.now().strftime("%d_%m_%Y__%H_%M_%S")
+    model_path = os.path.join(project_dir, "models", model_name)
+    model.save(model_path + ".h5")
+    tflite_model = lite.TFLiteConverter.from_keras_model(model).convert()
+    open(model_path + ".tflite", "wb").write(tflite_model)
 
-end = datetime.now()
-delta = end - start
-print("INFO: Training completed in {}".format(str(delta)))
+    # Evaluate the model on the test data using `evaluate`
+    print('\n# Evaluate on test data')
+    results = model.evaluate(validate)
+    print('test loss, test acc:', results)
+
+    print('\n# Generate predictions for 3 samples')
+    print("INFO: Predictions is {}".format(model.predict(validate)))
+    print('predictions shape:', predictions.shape)
+
+
+def main():
+    print("INFO: Training started with size {}".format(image_size))
+
+    print(tf.config.experimental.list_physical_devices(device_type=None))
+    for gpu in tf.config.experimental.list_physical_devices(device_type='GPU'):
+        tf.config.experimental.set_memory_growth(gpu, True)
+
+    start = datetime.now()
+    print("INFO: Training started with image size {} and batch size {}".format(image_size, batch_size))
+    train_model()
+    end = datetime.now()
+    delta = end - start
+    print("INFO: Training completed in {}".format(str(delta)))
+
+
+if __name__ == "__main__":
+    main()
